79c8896625cb859c88bf5365266b94e664a43afe
venku <venku@instance-1.asia-south1-a.c.lofty-stack-259514.internal>
2020-06-24 13:29:52 +0530

    mail server

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var app = express();
+const cors = require("cors");
+const expressValidator = require("express-validator");
+var bodyParser = require("body-parser");
+require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use(cors());
+app.options("*", cors());
+
+var port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.json({ data: "Welcome" });
+});
+
+app.post("/api/send-mail", async (req, res) => {
+  try {
+    req.assert("email", "Please provide a valid email.").isEmail();
+    req.assert("name", "Please provide a valid name.").notEmpty();
+    req.assert("message", "Please provide a message").notEmpty();
+    const errors = req.validationErrors();
+
+    if (errors) {
+      throw new Error(errors[0].msg);
+    }
+
+    const msg = {
+      to: "venkatesh.sv645@gmail.com",
+      from: req.body.email,
+      subject: "My-Portfolio - Contact",
+      text: req.body.message,
+    };
+
+    try {
+      await sgMail.send(msg);
+    } catch (error) {
+      debugger;
+    }
+
+    res.json({ message: "Thank you for your message, will get back to you." });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+app.listen(port);
+console.log("Server started at: localhost:" + port);
